1dfaf721b9ed236fa3f9858b44e08efeb7f183b2
jellychoco <ma88082@gmail.com>
2024-08-18 19:10:00 -0700

    remove useless log, improve shell

--- Server/start.sh
+++ Server/start.sh
@@ -11,3 +11,19 @@ fi
 
 node logMainServer.js &
 node script.js &
+
+sleep 5
+
+# Open the HTML file in the default browser
+if which xdg-open > /dev/null
+then
+  xdg-open http://127.0.0.1:5500/Front/index.html
+elif which gnome-open > /dev/null
+then
+  gnome-open http://127.0.0.1:5500/Front/index.html
+elif which open > /dev/null
+then
+  open http://127.0.0.1:5500/Front/index.html
+else
+  echo "Could not detect the web browser to open the HTML file."
+fi
